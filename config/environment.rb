5a3f3621cae96226655521f6f3a2d7e5adedce47
freedomofrent <freedomofrent@0ce741cd-a472-484f-b65b-c59da735b498>
2008-01-25 06:11:25 +0000

    t
    git-svn-id: http://svn.dali.speedyrails.net/svnfor/freedomofrent/trunk@16 0ce741cd-a472-484f-b65b-c59da735b498

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-if ENV['RAILS_ENV'] == 'production'
-  RAILS_GEM_VERSION = '2.0.1' unless defined? RAILS_GEM_VERSION
-else
-  RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
-end
+#  RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
   
 
 # Bootstrap the Rails environment, frameworks, and default configuration
