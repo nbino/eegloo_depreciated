2b3a7b0a3f000159f89bd0e4509600b464c4f6b9
freedomofrent <freedomofrent@0ce741cd-a472-484f-b65b-c59da735b498>
2008-01-25 04:22:57 +0000

    first commit
    git-svn-id: http://svn.dali.speedyrails.net/svnfor/freedomofrent/trunk@3 0ce741cd-a472-484f-b65b-c59da735b498

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,9 +2,6 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
-
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '1174a11c7b95105dcb91028a5b1071ad'
+  # Pick a unique cookie name to distinguish our session data from others'
+  session :session_key => '_freedomofrent_session_id'
 end
