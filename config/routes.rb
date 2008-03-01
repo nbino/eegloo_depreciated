7a913f6df3de5c03acec9c26163b47e0faa886a6
freedomofrent <freedomofrent@0ce741cd-a472-484f-b65b-c59da735b498>
2008-03-01 08:59:01 +0000

    fixed up views
    git-svn-id: http://svn.dali.speedyrails.net/svnfor/freedomofrent/trunk@38 0ce741cd-a472-484f-b65b-c59da735b498

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-<<<<<<< .mine
 ActionController::Routing::Routes.draw do |map|
   map.resources :comments
 
@@ -7,6 +6,9 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :password
   
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  # [EP] does this need to be here?
+  map.connect "/listings", :controller => 'listings', :action => 'index'
+ 
  
   map.resources :listings, :has_many => [:visuals, :favorites, :bogus_flags, :broker_flags, :na_flags, :listing_comments], :has_one => [:listing_info]
   
@@ -49,57 +51,3 @@ ActionController::Routing::Routes.draw do |map|
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
-=======
-
-ActionController::Routing::Routes.draw do |map|
-  
-  map.resource :session
-  map.resource :password
-  
-  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
- 
-  # [EP] does this need to be here?
-  map.connect "/listings", :controller => 'listings', :action => 'index'
-  
-  map.resources :listings, :has_many => [:visuals, :favorites, :bogus_flags, :broker_flags, :na_flags], :has_one => [:listing_info]
-  
-  map.resources :users
-  
-  map.resources :favorites
-  
-
-  # The priority is based upon order of creation: first created -> highest priority.
-
-  # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
-
-  # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
-
-  # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-
-  # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
-  #   end
-
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => 'sessions', :action => 'new'
-
-  # See how all your routes lay out with "rake routes"
-
-  # Install the default routes as the lowest priority.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
-end
->>>>>>> .r36
